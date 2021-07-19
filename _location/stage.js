2551cf6519d304634e7c4a175ebce381814a3117
Kyle Hessel <KHessel@PENNONI.com>
2021-07-19 16:15:08 -0400

    Distance value is now calculated using northing, easting, and elevation. the original 2D distance can still be accessed.

--- _location/stage.js
+++ _location/stage.js
@@ -1,3 +1,7 @@
+/////////////////////////
+/// Begin Kyle's Code ///
+/////////////////////////
+
 // coordinate inputs
 let inputN = 469834.013;
 let inputE = 1295854.817;
@@ -12,26 +16,35 @@ for (i = 0; i < pano.getNodeIds().length; i = i + 1) {
     let thisNode = pano.getNodeIds()[i];
     let destN = Number(pano.getNodeUserdata(pano.getNodeIds()[i]).copyright); // store node northing in an easy-to-read variable
     let destE = Number(pano.getNodeUserdata(pano.getNodeIds()[i]).source); // store node easting in an easy-to-read variable
+    let destZ = Number(pano.getNodeUserdata(pano.getNodeIds()[i]).author); // store node elevation in an easy-to-read variable
     let nodeTitle = pano.getNodeUserdata(pano.getNodeIds()[i]).title;
     let customID = pano.getNodeUserdata(pano.getNodeIds()[i]).customnodeid;
 
-    // calculate vertical distance
+    // calculate northing distance
     let diffN = inputN - destN; // i originally wrapped this in Math.abs(), but these values get squared below so it doesn't matter.
 
-    // calculate horizontal distance
+    // calculate easting distance
     let diffE = inputE - destE;
 
+    // calculate elevation distance
+    let diffZ = inputZ - destZ;
+
     // use both of these distances to get the absolute distance between the two points with pythagorean thorem.
-    let distance = Math.sqrt((diffN * diffN) + (diffE * diffE));
+    let distance2D = Math.sqrt((diffN * diffN) + (diffE * diffE));
+
+    // use our elevation difference and our hypotneuse from the last line to get our 3D (true) distance, simply called distance.
+    let distance = Math.sqrt((diffZ * diffZ) + (distance2D * distance2D));
 
     // store this information in a new object that has our nearest neighbor and other related information.
     let output = {
         'node': thisNode,
         'custom': customID,
         'title': nodeTitle,
+        'distance2D': distance2D,
         'distance': distance,
         'northing': destN,
-        'easting': destE
+        'easting': destE,
+        'elevation': destZ
     }
 
     // push this object to our array we created.
@@ -48,9 +61,11 @@ let sortByNearest = distances.sort((a, b) => {
 let nearestNeighborNode = distances[0].node;
 let nearestNeighborCustom = distances[0].custom;
 let nearestNeighborTitle = distances[0].title;
+let nearestNeighborDistance2D = String(distances[0].distance2D.toFixed(4));
 let nearestNeighborDistance = String(distances[0].distance.toFixed(4));
 let nearestNeighborNorthing = String(distances[0].northing);
 let nearestNeighborEasting = String(distances[0].easting);
+let nearestNeighborElevation = String(distances[0].elevation);
 
 // print that node ID. this is our nearest neighbor!
 console.log("Nearest node: " + nearestNeighbor);
@@ -59,10 +74,10 @@ console.log("Nearest node: " + nearestNeighbor);
 console.log("Custom ID: " + nearestNeighborCustom);
 console.log("Title: " + nearestNeighborTitle);
 console.log("Distance from input: " + nearestNeighborDistance);
+console.log("Distance (2D only): " + nearestNeighborDistance2D);
 console.log("Northing: " + nearestNeighborNorthing);
 console.log("Easting: " + nearestNeighborEasting);
-
-
+console.log("Elevation: " + nearestNeighborElevation);
 
 
 /*// for testing - cycles through all nodes in a given tour and prints three of their user data fields.
